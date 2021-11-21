ca61877200066c194735803996c2aec1cf7110b8
Evgen <e.a.konstantinov.88@gmail.com>
2021-11-21 11:21:35 +0300

    zoom done

--- script.js
+++ script.js
@@ -1,57 +1,50 @@
-// const a = [8, 1, 1, 7, 4, 0];
-// const b = [5, 8, 5, 4, 8];
+// const lupa = document.querySelector(".zoom-img-cursor");
+// const board = document.querySelector(".zoom-img-block");
+// const zoomImg = document.querySelector(".zoom-img-overlay");
 
-// function testArray(a, b) {
-//   // Тут нужно написать решение
-//   let sumA = 0;
-//   for (let i = 0; i < a.length; i++) {
-//     sumA += a[i];
+// function setMousePosition(e) {
+//   const { clientX, clientY } = e;
+//   lupa.style.cssText = `left: ${clientX}px;top: ${clientY}px`;
+//   zoomImg.style.cssText = `background-position: ${clientX}, ${clientY}`;
+//   console.log(clientX, clientY);
+//   if (clientY > 400 || clientX > 400) {
+//     lupa.style.cssText = `display: none`;
 //   }
-//   let sumB = 0;
-//   for (let i = 0; i < b.length; i++) {
-//     sumB += b[i];
-//   }
-//   return sumA + sumB;
 // }
-// console.log(testArray(a, b));
 
-// const a = '1234';
-// const b = '56789';
-// let c = [...a, ...b];
-// c.push('ivanov');
-// c.reverse();
-// c.join(', ');
-
-// console.log(c);
-
-// function testArray(a, b) {
-//   // Тут нужно написать решение
-//   let c = [...a, ...b];
-//   c.unshift('Иванов');
-//   c.reverse();
-//   return c.join(', ');
-// }
-// let rs = testArray(a, b);
-// console.log(rs);
-
-// let a = '19 October 1909 10:27';
-// let b = '28 March 1909 00:59';
-
-// let c = new Date(a);
-// console.log(c);
-const lupa = document.querySelector('.zoom-img-cursor');
-const board = document.querySelector('.zoom-img-block');
-const zoomImg = document.querySelector('.zoom-img-overlay');
-
-function setMousePosition(e) {
-  const { clientX, clientY } = e;
-  lupa.style.cssText = `left: ${clientX}px;top: ${clientY}px`;
-  zoomImg.style.cssText = `background-position: ${clientX}, ${clientY}`;
-  console.log(clientX, clientY);
-  if (clientY > 400 || clientX > 400) {
-    lupa.style.cssText = `display: none`;
-  }
-}
-
-board.addEventListener('mousemove', setMousePosition);
+// board.addEventListener("mousemove", setMousePosition);
 // board.removeEventListener('mousemove', setMousePosition);
+
+let zoomer = (function () {
+  document.querySelector("#img-zoomer-box").addEventListener(
+    "mousemove",
+    function (e) {
+      let original = document.querySelector("#img-1"),
+        magnified = document.querySelector("#img-2"),
+        style = magnified.style,
+        x = e.pageX - this.offsetLeft,
+        y = e.pageY - this.offsetTop,
+        imgWidth = original.offsetWidth,
+        imgHeight = original.offsetHeight,
+        xperc = (x / imgWidth) * 100,
+        yperc = (y / imgHeight) * 100;
+
+      //lets user scroll past right edge of image
+      if (x > 0.01 * imgWidth) {
+        xperc += 0.15 * xperc;
+      }
+
+      //lets user scroll past bottom edge of image
+      if (y >= 0.01 * imgHeight) {
+        yperc += 0.15 * yperc;
+      }
+
+      style.backgroundPositionX = xperc - 9 + "%";
+      style.backgroundPositionY = yperc - 9 + "%";
+
+      style.left = x - 180 + "px";
+      style.top = y - 180 + "px";
+    },
+    false
+  );
+})();

9ab8e3677f15af3f41d299d3118991ce3437d0b5
Evgen <e.a.konstantinov.88@gmail.com>
2021-11-21 12:29:16 +0300

    zoom done

--- script.js
+++ script.js
@@ -16,35 +16,35 @@
 // board.removeEventListener('mousemove', setMousePosition);
 
 let zoomer = (function () {
-  document.querySelector("#img-zoomer-box").addEventListener(
-    "mousemove",
-    function (e) {
-      let original = document.querySelector("#img-1"),
-        magnified = document.querySelector("#img-2"),
-        style = magnified.style,
-        x = e.pageX - this.offsetLeft,
-        y = e.pageY - this.offsetTop,
-        imgWidth = original.offsetWidth,
-        imgHeight = original.offsetHeight,
-        xperc = (x / imgWidth) * 100,
-        yperc = (y / imgHeight) * 100;
+  document
+    .querySelector("#img-zoomer-box")
+    .addEventListener("mousemove", function (e) {
+      let original = document.querySelector("#img-1");
+      let magnified = document.querySelector("#img-2");
+      style = magnified.style;
+
+      x = e.pageX - this.offsetLeft; //получаем точку входа мыши, вычисляя позицию страницы за вычетом смещения элемента
+      y = e.pageY - this.offsetTop;
+
+      imgWidth = original.offsetWidth;
+      imgHeight = original.offsetHeight; //получаем исходный размер изображения и вычисляем процент, где находится наша мышь
+      xPercent = (x / imgWidth) * 100;
+      yPercent = (y / imgHeight) * 100;
 
       //lets user scroll past right edge of image
       if (x > 0.01 * imgWidth) {
-        xperc += 0.15 * xperc;
+        xPercent += 0.15 * xPercent;
       }
 
       //lets user scroll past bottom edge of image
       if (y >= 0.01 * imgHeight) {
-        yperc += 0.15 * yperc;
+        yPercent += 0.15 * yPercent;
       }
 
-      style.backgroundPositionX = xperc - 9 + "%";
-      style.backgroundPositionY = yperc - 9 + "%";
+      style.backgroundPositionX = xPercent - 9 + "%"; //Устанавливаю горизонтальный фон увеличенного изображения
+      style.backgroundPositionY = yPercent - 9 + "%"; //Устанавливаю вертикальный фон увеличенного изображения
 
-      style.left = x - 180 + "px";
+      style.left = x - 180 + "px"; // Перемещение увеличительного стекла движением мыши
       style.top = y - 180 + "px";
-    },
-    false
-  );
+    });
 })();

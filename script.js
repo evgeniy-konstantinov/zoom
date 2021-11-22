3428924a5b79350fce31c6258fa94bf89f13545d
evgeniy.konstantinov <evgeniy.konstantinov@powercode.us>
2021-11-22 15:54:27 +0200

    well done

--- script.js
+++ script.js
@@ -14,40 +14,56 @@
 
 // board.addEventListener("mousemove", setMousePosition);
 // board.removeEventListener('mousemove', setMousePosition);
+let IS_ON = false;
 
-let zoomer = (function () {
+document.querySelector('#img-2').addEventListener('mousedown', (e) => {
+  e.preventDefault();
+  IS_ON = true;
+  console.log(IS_ON);
+});
+
+document.querySelector('#img-2').addEventListener('mouseup', () => {
+  IS_ON = false;
+  console.log(IS_ON);
+});
+
+let zoomer = function () {
   document
-    .querySelector("#img-zoomer-box")
-    .addEventListener("mousemove", function (e) {
-      let original = document.querySelector("#img-1");
-      let magnified = document.querySelector("#img-2");
-      style = magnified.style;
+    .querySelector('#img-zoomer-box')
+    .addEventListener('mousemove', function (e) {
+      if (IS_ON) {
+        let original = document.querySelector('#img-1');
+        let magnified = document.querySelector('#img-2');
+        style = magnified.style;
 
-      //получаем точку входа мыши, вычисляя позицию страницы за вычетом смещения элемента
-      x = e.pageX - this.offsetLeft;
-      y = e.pageY - this.offsetTop;
+        //получаем точку входа мыши, вычисляя позицию страницы за вычетом смещения элемента
+        x = e.pageX - this.offsetLeft;
+        y = e.pageY - this.offsetTop;
 
-      //получаем исходный размер изображения и вычисляем процент, где находится наша мышь
-      imgWidth = original.offsetWidth;
-      imgHeight = original.offsetHeight;
-      xPercent = (x / imgWidth) * 100;
-      yPercent = (y / imgHeight) * 100;
+        //получаем исходный размер изображения и вычисляем процент, где находится наша мышь
+        imgWidth = original.offsetWidth;
+        imgHeight = original.offsetHeight;
+        xPercent = (x / imgWidth) * 100;
+        yPercent = (y / imgHeight) * 100;
 
-      //позволяет пользователю прокручивать за правый край изображения
-      if (x > 0.01 * imgWidth) {
-        xPercent += 0.15 * xPercent;
+        //позволяет пользователю прокручивать за правый край изображения
+        if (x > 0.01 * imgWidth) {
+          xPercent += 0.15 * xPercent;
+        }
+
+        //позволяет пользователю прокручивать за нижний край изображения
+        if (y >= 0.01 * imgHeight) {
+          yPercent += 0.15 * yPercent;
+        }
+
+        style.backgroundPositionX = xPercent - 9 + '%'; //Устанавливаю горизонтальный фон увеличенного изображения
+        style.backgroundPositionY = yPercent - 9 + '%'; //Устанавливаю вертикальный фон увеличенного изображения
+
+        // Перемещение увеличительного стекла движением мыши
+
+        style.left = x - 180 + 'px';
+        style.top = y - 180 + 'px';
       }
-
-      //позволяет пользователю прокручивать за нижний край изображения
-      if (y >= 0.01 * imgHeight) {
-        yPercent += 0.15 * yPercent;
-      }
-
-      style.backgroundPositionX = xPercent - 9 + "%"; //Устанавливаю горизонтальный фон увеличенного изображения
-      style.backgroundPositionY = yPercent - 9 + "%"; //Устанавливаю вертикальный фон увеличенного изображения
-
-      // Перемещение увеличительного стекла движением мыши
-      style.left = x - 180 + "px";
-      style.top = y - 180 + "px";
     });
-})();
+};
+zoomer();
